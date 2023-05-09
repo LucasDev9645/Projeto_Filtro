a74f46f9c315503362273228df081af60e12b009
LucasDev <LucasDev9645@gmail.com>
2023-05-09 14:59:58 -0300

    Dynamic listing

--- src/components/ListScreen/ListingCard/index.tsx
+++ src/components/ListScreen/ListingCard/index.tsx
@@ -1,21 +1,17 @@
 import "./style.css";
 
+import * as request from "../../../util/request";
+
 const ListingCard = () => {
   return (
     <div className="listingCard-container">
       <ul>
-        <li>
-          <p>PC Gamer Pro</p>
-          <h4>R$ 1200.00</h4>
-        </li>
-        <li>
-          <p>PC Gamer Pro</p>
-          <h4>R$ 1200.00</h4>
-        </li>
-        <li>
-          <p>PC Gamer Pro</p>
-          <h4>R$ 1200.00</h4>
-        </li>
+        {request.findByPrice(0, 1500).map((product) => (
+          <li key={product.id}>
+            <p>{product.name}</p>
+            <h4>R$ {product.price}</h4>
+          </li>
+        ))}
       </ul>
     </div>
   );
